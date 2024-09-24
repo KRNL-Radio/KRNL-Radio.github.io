2a7bc602483cb6d9b0fba320169d7e4330adcf11
Luna <itsactuallyluna9@gmail.com>
2024-09-23 19:32:08 -0500

    add markdown support, add shows page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        print: { raw: "print" },
+        screen: { raw: "screen" },
+      },
+    },
   },
   plugins: [],
 };
