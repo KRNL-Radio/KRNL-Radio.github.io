2a65f472dbd598eb26d49b3e593cb7d33f1c8c74
Luna <itsactuallyluna9@gmail.com>
2025-09-24 21:07:47 -0500

    upgrade tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://krnl-radio.github.io",
-  integrations: [tailwind(), react()],
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  }
 });
